c92e1e1c80dd8ecf03088d751782ff72870701c4
Sekkacchi <sekkacchi@gmail.com>
2023-01-17 13:35:23 -0600

    Add try/catch block to each user route for asynchronous error handling.

--- src/v1/routes/users.ts
+++ src/v1/routes/users.ts
@@ -1,81 +1,114 @@
 import { PrismaClient } from "@prisma/client";
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 
 import * as Types from "../types/users";
 
 export const userRouter_v1 = express.Router();
 const prisma = new PrismaClient();
 
-userRouter_v1.get("/", async (request: Request, response: Response) => {
-  // throw new Error("This is a test error");
-  const users = await prisma.user.findMany({
-    include: { overview: true, logbooks: true, logbookReviews: true },
-  });
-  response.json(users);
-});
+userRouter_v1.get(
+  "/",
+  async (request: Request, response: Response, next: NextFunction) => {
+    // throw new Error("This is a test error");
+    try {
+      const users = await prisma.user.findMany({
+        include: { overview: true, logbooks: true, logbookReviews: true },
+      });
+      response.json(users);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
-userRouter_v1.get("/:userId", async (request: Request, response: Response) => {
-  const user = await prisma.user.findFirstOrThrow({
-    where: { id: Number(request.params.userId) },
-    include: { overview: true, logbooks: true, logbookReviews: true },
-  });
-  response.json(user);
-});
+userRouter_v1.get(
+  "/:userId",
+  async (request: Request, response: Response, next: NextFunction) => {
+    try {
+      const user = await prisma.user.findFirstOrThrow({
+        where: { id: Number(request.params.userId) },
+        include: { overview: true, logbooks: true, logbookReviews: true },
+      });
+      response.json(user);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
-userRouter_v1.post("/", async (request: Request, response: Response) => {
-  const userCreateData: Types.UserCreateData = {
-    email: request.body.email,
-    username: request.body.username,
-    firstName: request.body.firstName,
-    lastName: request.body.lastName,
-    birthday: request.body.birthday,
-    currency: request.body.currency,
-  };
-  const user = await prisma.user.create({ data: userCreateData });
-  response.json(user);
-});
+userRouter_v1.post(
+  "/",
+  async (request: Request, response: Response, next: NextFunction) => {
+    try {
+      const userCreateData: Types.UserCreateData = {
+        email: request.body.email,
+        username: request.body.username,
+        firstName: request.body.firstName,
+        lastName: request.body.lastName,
+        birthday: request.body.birthday,
+        currency: request.body.currency,
+      };
+      const user = await prisma.user.create({ data: userCreateData });
+      response.json(user);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 userRouter_v1.patch(
   "/:userId",
-  async (request: Request, response: Response) => {
-    const userPatchData: Types.UserUpdateData = {
-      email: request.body.email,
-      username: request.body.username,
-      firstName: request.body.firstName,
-      lastName: request.body.lastName,
-      birthday: request.body.birthday,
-      currency: request.body.currency,
-      theme: request.body.theme,
-      mobileNumber: request.body.mobileNumber,
-    };
-    const user = await prisma.user.update({
-      where: { id: Number(request.params.userId) },
-      data: userPatchData,
-    });
-    response.json(user);
+  async (request: Request, response: Response, next: NextFunction) => {
+    try {
+      const userPatchData: Types.UserUpdateData = {
+        email: request.body.email,
+        username: request.body.username,
+        firstName: request.body.firstName,
+        lastName: request.body.lastName,
+        birthday: request.body.birthday,
+        currency: request.body.currency,
+        theme: request.body.theme,
+        mobileNumber: request.body.mobileNumber,
+      };
+      const user = await prisma.user.update({
+        where: { id: Number(request.params.userId) },
+        data: userPatchData,
+      });
+      response.json(user);
+    } catch (error) {
+      next(error);
+    }
   }
 );
 
 userRouter_v1.patch(
   "/:userId/totalMoneySavedToDate",
-  async (request: Request, response: Response) => {
-    const totalMoneySavedToDateData: Types.UserTotalMoneySavedToDate = {
-      totalMoneySavedToDate: request.body.totalMoneySavedToDate,
-    };
-    const user = await prisma.user.update({
-      where: { id: Number(request.params.userId) },
-      data: totalMoneySavedToDateData,
-    });
-    response.json(user);
+  async (request: Request, response: Response, next: NextFunction) => {
+    try {
+      const totalMoneySavedToDateData: Types.UserTotalMoneySavedToDate = {
+        totalMoneySavedToDate: request.body.totalMoneySavedToDate,
+      };
+      const user = await prisma.user.update({
+        where: { id: Number(request.params.userId) },
+        data: totalMoneySavedToDateData,
+      });
+      response.json(user);
+    } catch (error) {
+      next(error);
+    }
   }
 );
 
 userRouter_v1.delete(
   "/:userId",
-  async (request: Request, response: Response) => {
-    const user = await prisma.user.delete({
-      where: { id: Number(request.params.userId) },
-    });
-    response.json(user);
+  async (request: Request, response: Response, next: NextFunction) => {
+    try {
+      const user = await prisma.user.delete({
+        where: { id: Number(request.params.userId) },
+      });
+      response.json(user);
+    } catch (error) {
+      next(error);
+    }
   }
 );
