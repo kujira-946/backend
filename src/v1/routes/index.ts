2ef8ece535c3f8530c2bf826fe4ca1e96fdc3441
Sekkacchi <sekkacchi@gmail.com>
2023-04-12 13:21:59 -0500

    BUGFIX : Remove now-deleted onboarding routes import in routes/index.ts.

--- src/v1/routes/index.ts
+++ src/v1/routes/index.ts
@@ -1,5 +1,4 @@
 import { authRouter_v1 } from "./auth.routes";
-import { onboardingRouter_v1 } from "./onboarding.routes";
 import { usersRouter_v1 } from "./users.routes";
 import { purchasesRouter_v1 } from "./purchases.routes";
 import { overviewsRouter_v1 } from "./overviews.routes";
@@ -9,7 +8,6 @@ import { logbookEntriesRouter_v1 } from "./logbook-entries.routes";
 
 export {
   authRouter_v1,
-  onboardingRouter_v1,
   usersRouter_v1,
   purchasesRouter_v1,
   overviewsRouter_v1,
