76e487f2cbab38725a969d02615044dd0f15a5ad
Sekkacchi <sekkacchi@gmail.com>
2023-05-24 18:08:49 -0400

    MINOR PATCH : Refactor route exports.

--- src/v1/routes/index.ts
+++ src/v1/routes/index.ts
@@ -1,19 +1,8 @@
-import { authRouter_v1 } from "./auth.routes";
-import { usersRouter_v1 } from "./users.routes";
-import { purchasesRouter_v1 } from "./purchases.routes";
-import { overviewsRouter_v1 } from "./overviews.routes";
-import { overviewGroupsRouter_v1 } from "./overview-groups.routes";
-import { logbooksRouter_v1 } from "./logbooks.routes";
-import { logbookEntriesRouter_v1 } from "./logbook-entries.routes";
-import { bugReportRouter_v1 } from "./bug-report.routes";
-
-export {
-  authRouter_v1,
-  usersRouter_v1,
-  purchasesRouter_v1,
-  overviewsRouter_v1,
-  overviewGroupsRouter_v1,
-  logbooksRouter_v1,
-  logbookEntriesRouter_v1,
-  bugReportRouter_v1,
-};
+export * from "./auth.routes";
+export * from "./users.routes";
+export * from "./purchases.routes";
+export * from "./overviews.routes";
+export * from "./overview-groups.routes";
+export * from "./logbooks.routes";
+export * from "./logbook-entries.routes";
+export * from "./bug-report.routes";
