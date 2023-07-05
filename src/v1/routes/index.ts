3f13fd3053432a10165d59f57baf3e3617655e9c
Yun <putecchi@gmail.com>
2023-07-05 18:19:16 -0400

    Delete imports

--- src/v1/routes/index.ts
+++ src/v1/routes/index.ts
@@ -2,7 +2,5 @@ export * from "./auth.routes";
 export * from "./users.routes";
 export * from "./purchases.routes";
 export * from "./overviews.routes";
-export * from "./overview-groups.routes";
 export * from "./logbooks.routes";
-export * from "./logbook-entries.routes";
 export * from "./bug-report.routes";
